6dc04d0fa2a464817a22e3e5d42e6e3d980332ab
AcaciaMan <11488694+AcaciaMan@users.noreply.github.com>
2025-02-01 21:21:59 +0200

    createLogPatterns

---
+++ vscode/acacia-log/src/utils/createLogPatterns.ts
@@ -0,0 +1,44 @@
+import * as vscode from 'vscode';
+import * as fs from 'fs';
+import * as path from 'path';
+
+export function createLogPatterns() {
+  const workspaceFolders = vscode.workspace.workspaceFolders;
+  if (!workspaceFolders) {
+    vscode.window.showErrorMessage('No workspace folder found');
+    return;
+  }
+
+  const workspacePath = workspaceFolders[0].uri.fsPath;
+  const vscodeFolderPath = path.join(workspacePath, '.vscode');
+  const logPatternsFilePath = path.join(vscodeFolderPath, 'logPatterns.json');
+
+  if (!fs.existsSync(vscodeFolderPath)) {
+    fs.mkdirSync(vscodeFolderPath);
+  }
+
+  if (!fs.existsSync(logPatternsFilePath)) {
+    const defaultLogPatterns = {
+      logPatterns: {
+        error: {
+            regexp: 'ERROR',
+            regexpoptions: 'i',
+            bSearch: true
+        },
+        warn: {
+            regexp: 'WARN',
+            regexpoptions: 'i',
+            bSearch: true
+        },
+        info: {
+            regexp: 'INFO',
+            regexpoptions: 'i',
+            bSearch: true
+        }
+      }
+    };
+
+    fs.writeFileSync(logPatternsFilePath, JSON.stringify(defaultLogPatterns, null, 2));
+    vscode.window.showInformationMessage('logPatterns.json created in .vscode folder');
+  }
+}
